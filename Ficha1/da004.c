967fde23765d79637241fb5b95c8a26899ab0394
Bruno Pascoal <brunompp98@gmail.com>
2020-10-07 18:48:13 +0100

    Exercicio numero 4

--- Ficha1/da004.c
+++ Ficha1/da004.c
@@ -1,62 +1,70 @@
-  #include <stdio.h>
+#include <stdio.h>
 
-  int main(){
-    int t,k,soma = 0;
-    long long int n,y = 0,z=1;
-    scanf("%d\n",&t);
-    for(int i = 0; i < t; i++){
-      scanf("%lld ",&n);
-      scanf("%d\n",&k);
+void maior(long long int n, int k){
+  int soma = 0,y;
+  while (soma != k){
+    long long int n1 = n+1;
+    soma = 0;
+    while(n1 != 0){
+      y = n1 % 10;
+      n1 = n1 / 10;
+      soma += y;
+    }
+    n++;
+  }
+  printf("%lld\n",n);
+}
+void menor(int k){
+  long long int y,z = 1,n = 1;
+  int soma = 0;
+  int multiplier = k / 10;
+  if(multiplier >= 8){
+    for (int j = 0; j < multiplier+1; j++){
+      n = n * 10 + 9;
+      z *= 10;
+    }
+    while (soma != k-1){
+      long long int n1 = n;
       soma = 0;
-      if(n < k){
-          n = 1;
-          int multiplier = k / 10;
-          if(multiplier >= 8){
-            for (int j = 0; j < multiplier+1; j++){
-              n = n * 10 + 9;
-              z *= 10;
-            }
-            while (soma != k-1){
-              long long int n1 = n;
-              soma = 0;
-              while(n1 != 0){
-              y = n1 % 10;
-              n1 = n1 / 10;
-              soma += y;
-            }
-            n += z;
-          }
-            }else {
-              if(multiplier < 8){
-            for (int j = 0; j < multiplier; j++){
-              n = n * 10 + 9;
-              z *= 10;
-            }
-            while (soma != k-1){
-              long long int n1 = n;
-              soma = 0;
-              while(n1 != 0){
-              y = n1 % 10;
-              n1 = n1 / 10;
-              soma += y;
-            }
-            n += z;
-          }
-        }
+      while(n1 != 0){
+        y = n1 % 10;
+        n1 = n1 / 10;
+        soma += y;
       }
-    }else{
-        while (soma != k){
-          long long int n1 = n+1;
-          soma = 0;
-          while(n1 != 0){
+      n += z;
+    }
+  }else {
+    if(multiplier < 8){
+      for (int j = 0; j < multiplier; j++){
+        n = n * 10 + 9;
+        z *= 10;
+      }
+      while (soma != k-1){
+        long long int n1 = n;
+        soma = 0;
+        while(n1 != 0){
           y = n1 % 10;
           n1 = n1 / 10;
           soma += y;
         }
-        n++;
+        n += z;
       }
     }
-      printf("%lld\n",n);
-    }
-    return 0;
   }
+  printf("%lld\n",n);
+}
+int main(){
+  int t,k;
+  long long int n;
+  scanf("%d\n",&t);
+  for(int i = 0; i < t; i++){
+    scanf("%lld ",&n);
+    scanf("%d\n",&k);
+    if(n < k){
+      menor(k);
+    }else{
+      maior(n,k);
+    }
+  }
+  return 0;
+}
