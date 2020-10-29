ae305299165d7f3678802da85b6241e5bd314c27
Bruno Pascoal <brunompp98@gmail.com>
2020-10-29 15:38:31 +0000

    Exercicio 6

--- Ficha2/daa006.c
+++ Ficha2/daa006.c
@@ -1,73 +1,47 @@
 #include <stdio.h>
 #include <math.h>
-#include <stdbool.h>
 
-bool checkDist(double qx,double qy,double qx2,double qy2,double cx,double cy,double cr){
-    double centro1 = ((qx-cx)*(qx-cx) + (qy2-cy)*(qy2-cy)); //canto superior esquerdo
-    double centro2 = ((qx2-cx)*(qx2-cx) + (qy2-cy)*(qy2-cy)); //canto superior direito
-    double centro3 = ((qx-cx)*(qx-cx) + (qy-cy)*(qy-cy)); //canto inferior esquerdo
-    double centro4 = ((qx2-cx)*(qx2-cx) + (qy-cy)*(qy-cy)); //canto inferior direito
+double intersecao(double qx, double qy, double ql, double cx, double cy, double cr){
+    double closeX = fmax(qx,fmin(cx,qx+ql));
+    double closeY = fmax(qy,fmin(cy,qy+ql));
+    double distX = cx - closeX;
+    double distY = cy - closeY;
 
-    if(centro1 <= cr*cr && centro2 <= cr*cr && centro3 <= cr*cr && centro4 <= cr*cr){
-        return true;
-    }else {
-        return false;
-        }
+    double area = 0;
+
+    if(ql*ql < 0.0000009){
+        return ql*ql;
+    }
+
+    if((distX*distX)+(distY*distY) >= cr*cr){ //nao se juntam
+      return area;
+    }
+    else if(((cy+cr) <= (qy+ql)) && ((cx+cr) <= (qx+ql)) && ((cy-cr) >= (qy)) && ((cx-cr) >= (qx))){  //circulo dentro do quadrado
+        return M_PI*cr*cr;
+    }
+    else if((qx-cx)*(qx-cx) + (qy-cy)*(qy-cy) < cr*cr  // canto inferior esquerdo
+        && (qx+ql-cx)*(qx+ql-cx) + (qy+ql-cy)*(qy+ql-cy) < cr*cr  // canto superior direito
+        && (qx-cx)*(qx-cx) + (qy+ql-cy)*(qy+ql-cy) < cr*cr  // canto superior esquerdo
+        && (qx+ql-cx)*(qx+ql-cx) + (qy-cy)*(qy-cy) < cr*cr){  //quadrado dentro do circulo
+        return ql*ql;
+    }
+    else{      //recursao
+        area += intersecao(qx, qy+ql/2.0, ql/2.0, cx, cy, cr);        //canto superior esquerdo
+        area += intersecao(qx+ql/2.0, qy+ql/2.0, ql/2.0, cx, cy, cr);        //canto superior direito
+        area += intersecao(qx, qy, ql/2.0, cx, cy, cr);        //canto inferior esquerdo
+        area += intersecao(qx+ql/2.0, qy, ql/2.0, cx, cy, cr);        //canto inferior direito
+    }
+    return area;
 }
 
-bool checkLadeits(double qx,double qy,double qx2,double qy2,double cx,double cy,double cr){
-    double ladeits1 = cy+cr;
-    double ladeits2 = cx+cr;
-    double ladeits3 = cy-cr;
-    double ladeits4 = cx-cr;
-    if(qy2 >= ladeits1 && qx2 >= ladeits2 && qy <= ladeits3 && qx <= ladeits4){
-        return true;
-    }else{
-        return false;
-    }
-}
-
-double intersecao(double qx,double qy,double qx2,double qy2,double cx,double cy,double cr){
-    double closestX = fmaxf(qx,fminf(cx,qx2));
-    double closestY = fmaxf(qy,fminf(cy,qy2));
-
-    double distanceX = cx - closestX;
-    double distanceY = cy - closestY;
-
-    double ladoquadrado = qx2-qx;
-    
-    if(ladoquadrado*ladoquadrado < 0.000000000001){
-        return ladoquadrado*ladoquadrado;
-    }
-
-
-    if((distanceX * distanceX)+(distanceY * distanceY) >= (cr * cr)){
-        return 0;
-    }
-    if(checkDist(qx,qy,qx2,qy2,cx,cy,cr)){ 
-        return ladoquadrado*ladoquadrado;   //quadrado dentro do circulo
-    }
-    if(checkLadeits(qx,qy,qx2,qy2,cx,cy,cr)){
-        return M_PI*cr*cr;    //circulo dentro de quadrado
-    } 
-    else{
-        double area = 0;
-        area = area + intersecao(qx,qy + ladoquadrado/2.0,qx2 - ladoquadrado/2.0,qy2,cx,cy,cr); // quadrado superior esquerdo
-        area = area + intersecao(qx + ladoquadrado/2.0,qy + ladoquadrado/2.0,qx2,qy2,cx,cy,cr); // quadrado superior direito
-        area = area + intersecao(qx,qy,qx2 - ladoquadrado/2.0,qy2 - ladoquadrado/2.0,cx,cy,cr); // quadrado inferior esquerdo
-        area = area + intersecao(qx + ladoquadrado/2.0,qy,qx2,qy2 - ladoquadrado/2.0,cx,cy,cr); // quadrado inferior direito    
-        return area;   
-    }
-}
 
 int main(int argc, char const *argv[]){
-    int n;
-    double qx,qy,ql,cx,cy,cr;
-    scanf("%d\n",&n);
-    for(double i = 0;i < n; i++){
-        scanf("%lf %lf %lf %lf %lf %lf\n",&qx,&qy,&ql,&cx,&cy,&cr);
-        double inter = intersecao(qx,qy,qx+ql,qy+ql,cx,cy,cr);
-        printf("%.4lf\n", inter);
+    int N;
+    double qx, qy, ql, cx, cy, cr;
+    scanf("%d", &N);
+    for(int i = 0; i < N; i++){
+        scanf("%lf %lf %lf %lf %lf %lf", &qx, &qy, &ql, &cx, &cy, &cr);
+        printf("%.4lf\n", intersecao(qx, qy, ql, cx, cy, cr));
+        
     }
-    return 0;
 }
